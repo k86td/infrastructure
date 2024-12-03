a6222e5ae790994e420527be23116782f794aa64
Tristan Lepine <tristan.lepine@ebox.ca>
2024-12-03 17:30:36 -0500

    added test update script

--- common/base.nix
+++ common/base.nix
@@ -8,5 +8,43 @@
     nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
     time.timeZone = "America/Toronto";
+
+    # configure auto-update flake timers
+    systemd.timers."update-flake" = {
+      wantedBy = [ "timers.target" ];
+      timerConfig = {
+        Unit = "update-flake.service";
+        OnUnitActiveSec = "1m";
+      };
+    };
+
+    systemd.services."update-flake" = {
+      serviceConfig = {
+        Type = "oneshot";
+        User = "root";
+      };
+      script = ''
+        set -eu
+        pushd /etc/nixos
+        ${pkgs.git}/bin/git fetch
+
+        current_branch=$(${pkgs.git}/bin/git rev-parse --abbrev-ref HEAD)
+        upstream_branch="origin/$current_branch"
+
+        if ! ${pkgs.git}/bin/git rev-parse --verify "$upstream_branch" &> /dev/null; then
+          ${pkgs.coreutils}/bin/echo "no upstream breanch for $current_branch"
+          exit 1
+        fi
+
+        local_commit=$(${pkgs.git}/bin/git rev-parse "$current_branch")
+        remote_commit=$(${pkgs.git}/bin/git rev-parse "$upstream_branch")
+
+        if [ "$local_commit" != "$remote_commit" ]; then
+          ${pkgs.coreutils}/bin/echo "found upstream changes. updating..."
+        else
+          ${pkgs.coreutils}/bin/echo "up to date."
+        fi
+      '';
+    };
   };
 }
